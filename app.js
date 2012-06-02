45dc55058b925ce205217783da1f68f56e3340da
Random Afk <don7.sp4m.me@gmail.com>
2012-06-02 12:28:06 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var http = require('http'); //Loads http modules
 
-function start(){
-    function onRequest(request, response){
-    // console.log("Request received");
-    response.writeHead(200,{"Content-Type" : "text/plain"});
+function onRequest (request, response){
+    console.log("request received");
+    response.writeHead(200, {"Content-Type" : "text/plain"});
     response.write("Hello World");
     response.end();
-    }
-    
+}
+
+
+function start() {   
     http.createServer(onRequest).listen(80);
-     
 }
 
 exports.start = start;
